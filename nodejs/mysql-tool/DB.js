ad74133fedc4072793b8a9efe62003224b571c99
RURO <p26763067@yahoo.com.tw>
2019-06-24 14:22:34 +0800

    更新mysql-tool。

--- nodejs/mysql-tool/DB.js
+++ nodejs/mysql-tool/DB.js
@@ -153,7 +153,7 @@ module.exports = class DB {
 
   /**
    * 批次執行，方便用。
-   * @param {Array<{commandText, value,resultName}>} commandArray [{commandText, value}]，value可以是[變數,'值']或function:(results,index)=>return [results[0],'qwe'];。resultName代表result回傳的名稱，用來取代原本的索引。
+   * @param {Array<{commandText, value,resultName}>} commandArray [{commandText, value}]，value可以是[變數,'值']或function:(results,index)=>return [results[0],'qwe'];。resultName代表result回傳的名稱，用來當作新的參考。
    * @param {boolean} usePool 是否使用Pool，預設false。
    * @param {boolean} useTrans 是否使用交易，預設false。
    */
@@ -192,12 +192,11 @@ module.exports = class DB {
             commandArray[i].commandText,
             value
           );
-          // 如果有設定resultName，則使用resultName代替索引。
+          // 如果有設定resultName，則新增resultName當回傳參考。
           if (commandArray[i].resultName) {
             results[commandArray[i].resultName] = oneResult;
-          } else {
-            results[i] = oneResult;
           }
+          results.push(oneResult);
         }
         await this.commitTrans(conn);
       } catch (e) {
@@ -223,12 +222,11 @@ module.exports = class DB {
           conn,
           i == commandArray.length - 1
         );
-        // 如果有設定resultName，則使用resultName代替索引。
+        // 如果有設定resultName，則新增resultName當回傳參考。
         if (commandArray[i].resultName) {
           results[commandArray[i].resultName] = oneResult;
-        } else {
-          results[i] = oneResult;
         }
+        results.push(oneResult);
       }
     }
     return results;
