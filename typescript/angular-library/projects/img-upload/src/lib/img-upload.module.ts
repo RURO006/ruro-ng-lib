4baa4f58839d610b55e5acbc4184b6ca0869a3c6
RURO <p26763067@gmail.com>
2019-07-02 11:40:04 +0800

    angular-library新增img-upload工具。

---
+++ typescript/angular-library/projects/img-upload/src/lib/img-upload.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ImgUploadComponent } from './img-upload.component';
+
+@NgModule({
+  declarations: [ImgUploadComponent],
+  imports: [
+  ],
+  exports: [ImgUploadComponent]
+})
+export class ImgUploadModule { }
---
+++ typescript/angular-library/projects/img-upload/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of img-upload
+ */
+
+export * from './lib/img-upload.service';
+export * from './lib/img-upload.component';
+export * from './lib/img-upload.module';
