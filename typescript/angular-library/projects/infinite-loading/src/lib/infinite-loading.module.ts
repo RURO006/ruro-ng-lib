f72c4bfa04fc9c7652c9fc1619d5f5db51014324
RURO <p26763067@yahoo.com.tw>
2019-08-17 21:32:54 +0800

    新增infinite-loading套件。

---
+++ typescript/angular-library/projects/infinite-loading/src/lib/infinite-loading.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { InfiniteLoadingDirective } from "./infinite-loading.directive";
+
+@NgModule({
+  declarations: [InfiniteLoadingDirective],
+  imports: [],
+  exports: [InfiniteLoadingDirective]
+})
+export class InfiniteLoadingModule {}
---
+++ typescript/angular-library/projects/infinite-loading/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of infinite-loading
+ */
+
+export * from './lib/infinite-loading.directive';
+export * from './lib/infinite-loading.module';
