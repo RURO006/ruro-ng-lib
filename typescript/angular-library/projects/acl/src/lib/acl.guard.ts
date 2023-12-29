5ea8adcc55a4a419aaaaa1d5125636191bb2b515
RURO <p26763067@gmail.com>
2023-12-29 17:33:42 +0800

    angular更新: 16>17。

--- typescript/angular-library/projects/acl/src/lib/acl.guard.ts
+++ typescript/angular-library/projects/acl/src/lib/acl.guard.ts
@@ -1,52 +1,26 @@
-import { Injectable } from '@angular/core';
-import {
-    CanActivateChild,
-    ActivatedRouteSnapshot,
-    RouterStateSnapshot,
-    UrlTree,
-    Router,
-    CanActivate,
-} from '@angular/router';
-import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { Router, CanActivateFn, CanActivateChildFn } from '@angular/router';
 import { AclService } from './acl.service';
 
-@Injectable()
-export class AclGuard implements CanActivate, CanActivateChild {
-    constructor(protected router: Router, protected aclService: AclService) {}
-    canActivate(
-        next: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
-    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        return this.action(next, state);
-    }
+export const AclGuard: CanActivateFn | CanActivateChildFn = (route, state) => {
+    const router = inject(Router);
+    const aclService = inject(AclService);
 
-    canActivateChild(
-        next: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
-    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        return this.action(next, state);
+    // 沒有設定權限，可以直接通行
+    if (!('gaAcl' in route.data)) {
+        return true;
     }
-
-    action(
-        next: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
-    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        // 沒有設定權限，可以直接通行
-        if (!('gaAcl' in next.data)) {
-            return true;
+    // 有包含權限
+    const hasPermission = aclService.checkPermission(route.data['gaAcl'], route.data['gaAclType']);
+    if (hasPermission) {
+        return true;
+    } else {
+        // 沒有包含權限，跳轉至登入頁
+        // 儲存跳轉前的頁面
+        aclService.referrer = state.url;
+        if (route.data['gaAclUrl']) {
+            router.navigateByUrl(route.data['gaAclUrl']);
         }
-        // 有包含權限
-        const hasPermission = this.aclService.checkPermission(next.data['gaAcl'], next.data['gaAclType']);
-        if (hasPermission) {
-            return true;
-        } else {
-            // 沒有包含權限，跳轉至登入頁
-            // 儲存跳轉前的頁面
-            this.aclService.referrer = state.url;
-            if (next.data['gaAclUrl']) {
-                this.router.navigateByUrl(next.data['gaAclUrl']);
-            }
-            return false;
-        }
+        return false;
     }
-}
+};
--- typescript/angular-library/projects/acl/src/lib/acl.module.ts
+++ typescript/angular-library/projects/acl/src/lib/acl.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { AclService } from './acl.service';
-import {
-  AclDirective,
-  AclAutoDisableDirective,
-  AclClassDirective,
-} from './acl.directive';
-import { AclGuard } from './acl.guard';
+import { AclDirective, AclAutoDisableDirective, AclClassDirective } from './acl.directive';
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  providers: [AclService, AclGuard],
-  exports: [],
+    declarations: [],
+    imports: [],
+    providers: [AclService],
+    exports: [],
 })
 export class AclModule {}
 
 @NgModule({
-  declarations: [AclDirective, AclAutoDisableDirective, AclClassDirective],
-  imports: [],
-  providers: [],
-  exports: [AclDirective, AclAutoDisableDirective, AclClassDirective],
+    declarations: [AclDirective, AclAutoDisableDirective, AclClassDirective],
+    imports: [],
+    providers: [],
+    exports: [AclDirective, AclAutoDisableDirective, AclClassDirective],
 })
 export class AclDirectiveModule {}
